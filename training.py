68d2e3d2af913b551420c51010d2d8926a963186
Vincent Rambold <vincent@rambold.de>
2022-05-04 20:48:24 +0200

    Initial commit

---
+++ training.py
@@ -0,0 +1,17 @@
+from pykeen.triples import TriplesFactory
+
+from pykeen.pipeline import pipeline
+
+training_set = "datasets/FB15K-237.2/Release/train.txt"
+testing_set = "datasets/FB15K-237.2/Release/test.txt"
+val_set = "datasets/FB15K-237.2/Release/valid.txt"
+
+result = pipeline(
+    training=training_set,
+    testing=testing_set,
+    validation=val_set,
+    model='TransE',
+    epochs=5,  # short epochs for testing
+)
+print(result)
+result.save_to_directory("results")
